ab35ffaf5640fc60a324c6bd09e53f924e6f0a1a
Emerson <emersontec147@gmail.com>
2023-06-18 20:41:54 -0500

    FALTA EL EMAILJS NO ENVIA CORREOS

---
+++ src/components/canvas/index.js
@@ -0,0 +1,6 @@
+import StarsCanvas from './Stars';
+import ComputersCanvas from './Computers';
+import BallCanvas from './Ball.jsx';
+import EarthCanvas from './Earth'
+
+export { StarsCanvas, ComputersCanvas, BallCanvas, EarthCanvas }
---
+++ src/components/index.js
@@ -0,0 +1,26 @@
+import About from "./About";
+import Contact from "./Contact.jsx";
+import Experience from "./Experience";
+import Feedbacks from "./Feedbacks";
+import Hero from "./Hero";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import Tech from "./Tech";
+import Works from "./Works";
+
+import { ComputersCanvas, StarsCanvas, BallCanvas, EarthCanvas } from "./canvas";
+
+export {
+ About, 
+ Contact, 
+ Experience, 
+ Feedbacks, 
+ Hero, 
+ Loader, 
+ Navbar, 
+ Tech, 
+ Works,
+ComputersCanvas,
+StarsCanvas,
+BallCanvas,
+EarthCanvas}
